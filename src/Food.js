e8ab03612e04f5720453d99c2cf01777345fc2c1
Massi Saoudi <106273388+Maci96@users.noreply.github.com>
2024-01-16 12:25:37 +0100

    Update Food.js

--- src/Food.js
+++ src/Food.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./index.css";
-export default (props) => {
+export default const  Food=(props) => {
   const style = {
     top: `${props.dot[0]}%`,
     left: `${props.dot[1]}%`,
   };
-  console.log(props.dot);
   return <div className="snake-food" style={style}></div>;
 };
